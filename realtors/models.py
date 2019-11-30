659181f219a06b0b69e0c4c27c9eb998c22e4bcd
Blagoj5 <blagoj.petrov5@gmail.com>
2019-12-01 00:35:04 +0100

    Fixing bugs that happend while migrating and styling the admin site

--- realtors/models.py
+++ realtors/models.py
@@ -6,9 +6,9 @@ class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
-    email = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=50)
     is_mvp =models.BooleanField(default=False)
-    hire_date = models.DateField(default=datetime)
+    hire_date = models.DateField(default=datetime.now, blank=True) 
     def __str__(self):
         return self.name
